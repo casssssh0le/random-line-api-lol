04b102c610f8ac122b892f3b2a5b144644ccc130
casssssh0le <ceahlgrim@icloud.com>
2025-05-09 12:06:38 -0400

    Create random-line.js

---
+++ api/random-line.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+module.exports = async function (req, res) {
+  const fileUrl = "https://gist.githubusercontent.com/casssssh0le/b6434170a35f866e9884a864530ecc94/raw/b24cc1c36e778bbb4b5d313bab2fcdeb79293b6b/jokes";
+
+  https.get(fileUrl, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const randomLine = lines[Math.floor(Math.random() * lines.length)];
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(randomLine);
+    });
+
+  }).on('error', (err) => {
+    console.error('Error fetching file:', err);
+    res.statusCode = 500;
+    res.end('Failed to fetch data.');
+  });
+};
